7b24cae3f385911ad7f91b6d2aa211905f2a5aa7
Henrikas Elsbergas <henrikas@henrikas-Inspiron-M5010.(none)>
2012-05-01 12:28:38 +0300

    translating to lt

---
+++ messages/lt/yiiadmin.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+    'Пожалуйста, исправьте ошибки, указанные ниже.' => 'Prašome pataisyti klaidas.',
+    'Изменения сохранены. Можете создать новую запись.' => 'Pakeitimai išsaugoti. Galite pridėti dar vieną įrašą.',
+    'Сохранить и создать новую запись' => 'Išsaugoti ir pridėti naują',
+    'Пожалуйста, укажите пароль.' => 'Prašome įvesti slaptažodį.',
+    'Сохранить и редактировать' => 'Išsaugoti ir tęsti redagavimą',
+    'Изменения сохранены.' => 'Pakeitimai išsaugoti',
+    'Удалить запись ID ' => 'Ištrinamo įrašo ID',
+    'Администрирование' => 'Administracija',
+    'Запись создана.' => 'Įrašas sukurtas',
+    'Список Моделей' => 'Įrašų sąrašas',
+    'Редактировать' => 'Redaguoti',
+    'Сохранить' => 'Išsaugoti',
+    'Изменить' => 'Pakeisti',
+    'Удалить' => 'Ištrinti',
+    'Главная' => 'Namai',
+    'Создать' => 'Sukurti',
+    'Выход' => 'Atsijungti',
+    'Вход' => 'Įeiti',
+);
